d242fbf8a0e27b64440ea9e0764b99537a64e330
Anderson Mello <andersonmello722@gmail.com>
2022-07-07 17:16:02 -0300

    Requisito 3

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Header from './Header';
 
 class Home extends React.Component {
   render() {
     return (
       <div>
+        <Header />
         <p data-testid="home-initial-message">
           Digite algum termo de pesquisa ou escolha uma categoria.
         </p>
