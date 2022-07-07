14e0fa6dfb0cca234bc9db80ada52d02d58fda59
Anderson Mello <andersonmello722@gmail.com>
2022-07-07 16:12:21 -0300

    Requisito 1

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { getCategories } from './services/api';
 
 function App() {
+  getCategories();
   return (
     <div className="App">
       <header className="App-header">
